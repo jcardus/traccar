eb4d30aca21a6a1fe8361ff57118dff414ee163d
jcardeira <joaquim.cardeira@gmail.com>
2026-04-06 06:41:00 +0100

    fix destination

--- src/main/java/org/traccar/forward/NetworkForwarder.java
+++ src/main/java/org/traccar/forward/NetworkForwarder.java
@@ -48,8 +48,8 @@ public class NetworkForwarder {
 
     public void forward(InetSocketAddress source, int port, boolean datagram, byte[] data) {
         try {
+            destination = InetAddress.getByName(port == 5023 ? "nt20.stc.srv.br" : "jsrastreamento.stctecnologia.com.br");
             if (port == 5023) {
-                destination = InetAddress.getByName("nt20.stc.srv.br");
                 port = 10107;
             }
             if (datagram) {
