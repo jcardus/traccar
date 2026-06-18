9698defc5178fc66c716717c067d16a3ae2b19ff
Joaquim Cardeira <joaquim.cardeira@gmail.com>
2026-06-18 19:42:17 +0100

    kms in vl100 odometer

--- src/main/java/org/traccar/protocol/Gt06ProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/Gt06ProtocolDecoder.java
@@ -1027,7 +1027,7 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
             }
 
             if (modelNT20 && type == MSG_GPS_LBS_2) {
-                position.set(Position.KEY_ODOMETER, buf.readUnsignedMedium());
+                position.set(Position.KEY_ODOMETER, buf.readUnsignedMedium() * 1000L);
                 position.set(Position.KEY_HOURS, buf.readUnsignedMedium() * 60 * 1000L);
             }
 
